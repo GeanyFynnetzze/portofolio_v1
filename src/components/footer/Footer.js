b9e7281ec175ccd4f6e7cc19c242c952d6da9cdf
Catalin <catalinalbert@yahoo.com>
2022-03-06 14:11:53 +0200

    finishing last section(footer)

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import './footer.css';
 function Footer() {
-  return <>FOOTER HERE</>;
+  return (
+    <footer>
+      <a href="#" className="footer--logo">
+        TANK
+      </a>
+      <ul className="footer--permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Expereince</a>
+        </li>
+        <li>
+          <a href="#portofolio">Portofolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <h3 className="footer--copyright">TankIndustries©All Rights Reserved</h3>
+    </footer>
+  );
 }
 
 export default Footer;
